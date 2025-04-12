ba126dcc3d1a072b294160891ce8c379c677e95b
Sameer Ali <sameeralifazal@gmail.com>
2025-04-12 11:47:33 +0500

    Updated the UI(CSS) and added localstorage functionality to store the dataconst taskListForm = document.getElementById('taskListForm')

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,14 @@
-let tasks = [];
-
 const taskListForm = document.getElementById('taskListForm')
 const addTaskBtn = document.getElementById('addTaskBtn')
 const addTaskInput = document.getElementById('addTaskInput')
 const tasksList = document.getElementById('tasksList')
 
+
 class taskObj {
     constructor(task_name) {
         this.id = Date.now();
         this.task_name = task_name;
-        let isCompleted = false;
-        this.isCompleted = isCompleted;
+        this.isCompleted = false;
     };
 
     edit_task(newTask) {
@@ -22,18 +20,39 @@ class taskObj {
     }
 }
 
+
+const savetasks = (tasksarr) => {
+    localStorage.setItem('tasks', JSON.stringify(tasksarr));
+}
+
+const gettasks = () => {
+    if (localStorage.getItem('tasks') == null) return []
+    else {
+        let tasksarr = JSON.parse(localStorage.getItem('tasks'))
+        return tasksarr.map(task => {
+            let newtask = new taskObj(task.task_name);
+            newtask.id = task.id;
+            newtask.isCompleted = task.isCompleted;
+            return newtask;
+        })
+    }
+}
+
+let tasks = gettasks();
+
 const addTask = (e) => {
     e.preventDefault();
     let task = new taskObj(addTaskInput.value);
     addTaskInput.value = ''
     tasks.push(task)
+    savetasks(tasks)
     displayTasks(tasks)
 }
 
 const displayTasks = (arr) => {
-    if (tasks.length = 0) {
+    if (tasks.length == 0) {
         tasksList.innerHTML = '<h2>No Tasks Added yet</h2>';
-    } else if(tasks.length > 0){
+    } else if (tasks.length > 0) {
         tasksList.innerHTML = '';
         arr.forEach(element => {
             let taskItem = document.createElement('li')
@@ -56,6 +75,7 @@ const displayTasks = (arr) => {
             if (element.isCompleted) {
                 checkbox.checked = true
                 taskContent.style.textDecoration = 'line-through'
+                taskContent.style.color = 'green'
             };
             checkbox.addEventListener('click', markComplete)
             let deleteTaskbtn = taskItem.querySelector('.deleteTaskbtn')
@@ -71,8 +91,9 @@ function editTask(e) {
     if (task) {
         let newTask = prompt('Enter New Task', task.task_name);
         if (newTask !== null && newTask.trim() !== '') {
-            task.edit_task(newTask)
-            displayTasks(tasks)
+            task.edit_task(newTask);
+            savetasks(tasks);
+            displayTasks(tasks);
         }
     }
 }
@@ -82,6 +103,7 @@ function markComplete() {
     let task = tasks.find(task => task.id == id);
     if (task) {
         task.edit_state()
+        savetasks(tasks)
         displayTasks(tasks)
     }
 }
@@ -89,8 +111,9 @@ function markComplete() {
 function deleteTask() {
     let id = this.parentElement.parentElement.getAttribute('id')
     let index = tasks.indexOf(tasks.find(task => task.id == id), 0)
-    if (index !== -1 & index >= 0) {
+    if (index !== -1 && index >= 0) {
         tasks.splice(index, 1)
+        savetasks(tasks)
         displayTasks(tasks)
     }
 }
